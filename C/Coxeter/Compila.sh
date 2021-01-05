ef3df9c296f11832edf1a5a70016150d1fdda69f
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2021-01-05 16:30:30 +0100

    Add files via upload

---
+++ C/Coxeter/Compila.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+gcc gtnauty.c gtools.c gutil1.c gutil2.c naugraph.c naugroup.c naurng.c nausparse.c nautil.c nautinv.c naututil.c nauty.c nautycliquer.c schreier.c traces.c nautyex_coxeter.c -o nautyex_coxeter
+
