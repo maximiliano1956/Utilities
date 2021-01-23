f773cb579248cbebcf868a48c28eb504daded791
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2021-01-23 22:21:51 +0100

    Add files via upload

---
+++ C/SteinerSystems/Compila.sh
@@ -0,0 +1,4 @@
+#gcc -DWFILE -DWDEBUG -g SteinerSystems.c dlx.c -o SteinerSystems
+#gcc -g SteinerSystems.c dlx.c -o SteinerSystems
+gcc -DWFILE -g SteinerSystems.c dlx.c -o SteinerSystems
+gcc -g debugSteiner.c dlx.c -o debugSteiner
