caaa6dbb6d2ff1fc003ffd8a77e21315fa359ef4
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2021-01-24 22:04:18 +0100

    Delete Compila.sh

--- C/SteinerSystems/Compila.sh
+++
@@ -1,4 +0,0 @@
-#gcc -DWFILE -DWDEBUG -g SteinerSystems.c dlx.c -o SteinerSystems
-#gcc -g SteinerSystems.c dlx.c -o SteinerSystems
-gcc -DWFILE -g SteinerSystems.c dlx.c -o SteinerSystems
-gcc -g debugSteiner.c dlx.c -o debugSteiner
