cc09ff15b6ac1b0cbeacb00c9b323d600711a463
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2021-02-04 16:43:10 +0100

    Delete Compila_STS_2-5-21.sh

--- C/MieiEsempi/Compila_STS_2-5-21.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-gcc -g gtnauty.c gtools.c gutil1.c gutil2.c naugraph.c naugroup.c naurng.c nausparse.c nautil.c nautinv.c naututil.c nauty.c nautycliquer.c schreier.c traces.c nautyex_STS_2-5-21.c -o nautyex_STS_2-5-21
-
