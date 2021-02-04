1510aa1a0c15cd18cb541372eeeaac2f99f0fc64
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2021-02-04 16:43:25 +0100

    Delete Compila_desargues.sh

--- C/MieiEsempi/Compila_desargues.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-gcc -g gtnauty.c gtools.c gutil1.c gutil2.c naugraph.c naugroup.c naurng.c nausparse.c nautil.c nautinv.c naututil.c nauty.c nautycliquer.c schreier.c traces.c nautyex_desargues.c -o nautyex_desargues
-
