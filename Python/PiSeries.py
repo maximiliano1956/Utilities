487315169d17944bee272d9582d2d4c95d9cf8d4
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2024-12-13 20:05:50 +0100

    Add files via upload

---
+++ Python/PiSeries.py
@@ -0,0 +1,99 @@
+from math import pi as Pi
+from numpy import tan as Tan
+
+
+def sum():
+
+    return 1/a-Pi/(b*Tan(Pi*a/b))
+
+
+
+def f():
+    
+    if n==a:
+        return 0
+
+    m=n%b
+
+    if m==b-a:
+
+        return 1
+
+    if m==a:
+
+        return -1
+
+    return 0
+
+
+
+"""
+
+DEVE ESSERE a<b con a,b interi > 0
+
+"""
+
+a=40
+
+b=100
+
+nloop=1000
+
+serie=0
+
+for from math import pi as Pi
+from numpy import tan as Tan
+
+
+def sum():
+
+    return 1/a-Pi/(b*Tan(Pi*a/b))
+
+
+
+def f():
+    
+    if n==a:
+        return 0
+
+    m=n%b
+
+    if m==b-a:
+
+        return 1
+
+    if m==a:
+
+        return -1
+
+    return 0
+
+
+
+"""
+
+DEVE ESSERE a<b con a,b interi > 0
+
+"""
+
+a=40
+
+b=100
+
+nloop=1000
+
+serie=0
+
+for n in range(1,nloop):
+
+    serie=serie+f()/n
+
+    
+
+print(serie,sum())n in range(1,nloop):
+
+    serie=serie+f()/n
+
+    
+
+print(serie,sum())
