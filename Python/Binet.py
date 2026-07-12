a0a80c40f660b5ee657c5af67c4e8f77fd837a9d
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2026-07-12 03:07:43 +0200

    Add files via upload

---
+++ Python/Binet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+
+f1=(1-math.sqrt(5))/2
+f2=(1+math.sqrt(5))/2
+
+def f(k):
+    return 1/math.sqrt(5)*(f2**k-f1**k)
+
+n=10
+
+for i in range(0,11):
+    print("F(",i,")=",int(f(i)))
