97d31e5a424788bb51a4b2632f35a3bed4129fab
Lelli Massimo <35976219+maximiliano1956@users.noreply.github.com>
2026-09-15 05:38:10 +0200

    Fix formatting in Partridge.c output

--- C/Partridge/Partridge.c
+++ C/Partridge/Partridge.c
@@ -1125,7 +1125,7 @@ unsigned int WFSol(char *solname) {
 			int bd=field1[row][col];
 
 			if (col<size-1)
-				fprintf(fp,"%02d,",bd-2);
+				fprintf(fp,"%02d ",bd-2);
 			else
 				fprintf(fp,"%02d\n",bd-2);
 		}
